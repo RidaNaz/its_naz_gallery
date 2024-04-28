97966403b0e35ea8fb21eec967f9f8820cc31a77
RidaNaz <rnaz3414@gmail.com>
2024-04-28 10:08:00 +0500

    commit by Rida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 "use client"
 
 import Footer from "@/components/Footer"
+import { MobNav } from "@/components/MobNav"
 import Spline from "@splinetool/react-spline"
 
 export default function Home() {
   return (
 
-    <main className="flex flex-col" >
-      <div className="flex flex-col sm:pt-32 md:flex-row md:pr-4 mx-10 sm:pr-4 lg:pr-2 sm:-mx-24 md:-mx-28 lg:mx-0 w-full h-screen items-center justify-center relative overflow-x-hidden pb-12">
+    <main className="mt-36 sm:mt-0 flex flex-col" >
+      <MobNav />
+
+      <div className="flex flex-col -mt-32 sm:-mt-0 sm:pt-32 md:flex-row md:pr-4 mx-4 sm:pr-4 lg:pr-2 sm:-mx-24 md:-mx-28 lg:mx-0 w-full h-screen items-center justify-center relative overflow-x-hidden pb-12">
         <div className=" w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start">
 
-          <div className="flex flex-col ml-4 gap-2">
+          <div className="flex flex-col ml-4 gap-2 -mb-36 sm:-mb-0">
             <h1 className="text-4xl font-black md:text-6xl text-cyan-500 ">Naz Gallery</h1>
             <h2 className="text-md md:text-base lg:text-[20px]">Welcome to my World!</h2>
           </div>
